bd52edcae9df6cc9eaf9f5d10722312a3395b394
jcullina <jcullina@redhat.com>
2026-04-16 15:23:11 +0100

    Exercise - show fave icon on movie details page

--- src/components/HeaderMovie.tsx
+++ src/components/HeaderMovie.tsx
@@ -5,6 +5,7 @@ import Paper from "@mui/material/Paper";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import HomeIcon from "@mui/icons-material/Home";
+import FavoriteIcon from "@mui/icons-material/Favorite"; // 1. Added Import
 import { MovieDetailsProps } from "../types/movieAppTypes"; 
 
 const styles = {
@@ -17,22 +18,32 @@ const styles = {
   },
 };
 
-const MovieHeader= (movie: MovieDetailsProps) => {
-  
+const MovieHeader = (movie: MovieDetailsProps) => {
+  const favourites = JSON.parse(localStorage.getItem("favourites") || '[]');
+  const isFavourite = favourites.some((m: any) => m.id === movie.id);
+
   return (
     <Paper component="div" sx={styles.root}>
       <IconButton aria-label="go back">
         <ArrowBackIcon color="primary" fontSize="large" />
       </IconButton>
 
+      {/* Logic moved here: between the back button and the title group */}
+      {isFavourite && (
+        <FavoriteIcon color="error" fontSize="large" />
+      )}
+
       <Typography variant="h4" component="h3">
         {movie.title}{"   "}
         <a href={movie.homepage}>
-          <HomeIcon color="primary"  fontSize="large"/>
+          <HomeIcon color="primary" fontSize="large"/>
         </a>
         <br />
-        <span>{`${movie.tagline}`} </span>
+        <Typography variant="subtitle1" component="span">
+          {movie.tagline}
+        </Typography>
       </Typography>
+
       <IconButton aria-label="go forward">
         <ArrowForwardIcon color="primary" fontSize="large" />
       </IconButton>
