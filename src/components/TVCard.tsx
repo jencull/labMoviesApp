5c8be270aa17978807721a27d132f14fefdf2ea5
jcullina <jcullina@redhat.com>
2026-05-07 16:54:01 +0100

    PT-2-3 completed: add TVCard component

---
+++ src/components/TVCard.tsx
@@ -0,0 +1,71 @@
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import CardHeader from "@mui/material/CardHeader";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import CalendarIcon from "@mui/icons-material/CalendarTodayTwoTone";
+import StarRateIcon from "@mui/icons-material/StarRate";
+import Grid from "@mui/material/Grid";
+import img from '../images/film-poster-placeholder.png';
+import { TVSeriesOverview } from "../types/movieAppTypes";
+import { Link } from "react-router-dom";
+
+const styles = {
+  card: { maxWidth: 345 },
+  media: { height: 500 },
+};
+
+interface TVCardProps {
+  tvSeries: TVSeriesOverview;
+  action: (tv: TVSeriesOverview) => React.ReactNode;
+}
+
+const TVCard = ({ tvSeries, action }: TVCardProps) => {
+  return (
+    <Card sx={styles.card}>
+      <CardHeader
+        title={
+          <Typography variant="h5" component="p">
+            {tvSeries.name}
+          </Typography>
+        }
+      />
+      <CardMedia
+        sx={styles.media}
+        image={
+          tvSeries.poster_path
+            ? `https://image.tmdb.org/t/p/w500/${tvSeries.poster_path}`
+            : img
+        }
+      />
+      <CardContent>
+        <Grid container>
+          <Grid item xs={6}>
+            <Typography variant="h6" component="p">
+              <CalendarIcon fontSize="small" />
+              {tvSeries.first_air_date}
+            </Typography>
+          </Grid>
+          <Grid item xs={6}>
+            <Typography variant="h6" component="p">
+              <StarRateIcon fontSize="small" />
+              {"  "} {tvSeries.vote_average}{" "}
+            </Typography>
+          </Grid>
+        </Grid>
+      </CardContent>
+      <CardActions disableSpacing>
+        {action(tvSeries)}
+        <Link to={`/tv/${tvSeries.id}`}>
+          <Button variant="outlined" size="medium" color="primary">
+            More Info ...
+          </Button>
+        </Link>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default TVCard;
